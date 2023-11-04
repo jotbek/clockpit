d63a125ecad64e951ea74ea2ac91ebb84165be8a
jotbek <jbarcewicz@gmail.com>
2023-11-04 18:20:10 +0100

    extend clock module

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from machine import Pin
 from time import sleep
 from neopixel import NeoPixel
 from mod_raybouncer import Bounce
+from mod_clock import Clock
 
 # 0 - 255
 intense = 8
@@ -24,11 +25,13 @@ def mapPixel(x, y):
 def light(x, y, r, g, b):
     wall[mapPixel(x, y)] = (int(intense*r/255), int(intense*g/255), int(intense*b/255))
 
-bounce = Bounce(xres, yres, 10, [[0, 0, 128], [128, 0, 0]])
+# mod = Bounce(xres, yres, 10, [[0, 0, 128], [128, 0, 0]])
+mod = Clock()
 
 while True:
-    changes = bounce.get()
+    changes = mod.get(4)
     print(changes)
+    
     for ch in changes:
         light(ch[0], ch[1], ch[2][0], ch[2][1], ch[2][2])
 
