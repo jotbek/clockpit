f1554a75824e1737f6a9587e4967c5bfe1e80ebb
jotbek <jbarcewicz@gmail.com>
2023-11-04 18:34:12 +0100

    clean main file

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ from neopixel import NeoPixel
 from mod_raybouncer import Bounce
 from mod_clock import Clock
 
+
 # 0 - 255
 intense = 8
-
-internal_pin = Pin("LED", Pin.OUT)
-internal_pin.on()
-
 xres = 16
 yres = 16
 pin = 22
 wall = NeoPixel(Pin(pin), xres * yres)
 wall.write()
+
 
 def mapPixel(x, y):
     if y % 2 == 1:
@@ -22,14 +20,15 @@ def mapPixel(x, y):
     else:
         return xres * y + xres - 1 - x
 
+
 def light(x, y, r, g, b):
     wall[mapPixel(x, y)] = (int(intense*r/255), int(intense*g/255), int(intense*b/255))
 
-# mod = Bounce(xres, yres, 10, [[0, 0, 128], [128, 0, 0]])
-mod = Clock()
+mod = Bounce(xres, yres, 10, [[0, 0, 128], [128, 0, 0]])
+# mod = Clock()
 
 while True:
-    changes = mod.get(4)
+    changes = mod.get()
     print(changes)
     
     for ch in changes:
@@ -38,6 +37,3 @@ while True:
     wall.write()
     sleep(0.08)
 
-sleep(1)
-internal_pin.off()
-print("Finished.")
