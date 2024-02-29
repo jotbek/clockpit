cdaaa7b2121281c8cba7bb6f6af99bfc84c377ed
jotbek <jbarcewicz@gmail.com>
2024-02-29 14:19:10 +0100

    Simplified settime module to use ntptime

--- settime.py
+++ settime.py
@@ -1,33 +1,19 @@
 import network
-import socket
 import time
-import struct
 import secrets
+import ntptime
 
 from machine import Pin
 from machine import RTC
 
-NTP_DELTA = 2208988800
-host = "pool.ntp.org"
-
 ssid = secrets.wifi_name
 password = secrets.wifi_pass
 
+
 def set_time():
-    NTP_QUERY = bytearray(48)
-    NTP_QUERY[0] = 0x1B
-    addr = socket.getaddrinfo(host, 123)[0][-1]
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        s.settimeout(1)
-        res = s.sendto(NTP_QUERY, addr)
-        msg = s.recv(48)
-    finally:
-        s.close()
-    val = struct.unpack("!I", msg[40:44])[0]
-    t = val - NTP_DELTA    
-    tm = time.gmtime(t)
-    RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0))
+    print("Local time before synchronization: %s" %str(time.localtime()))
+    ntptime.settime()
+    print("Local time after synchronization: %s" %str(time.localtime()))
 
 
 def run():
